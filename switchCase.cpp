0e4f02ecbf63b56474d4670ad8397df7e2eef9ec
kawsarabdisalan <113559449+kawsarabdisalan@users.noreply.github.com>
2024-12-06 16:36:09 -0500

    Add files via upload

---
+++ switchCase.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int month = 7;
+    switch (month) {
+        case 1:
+            cout << "Jan";
+            break;
+        case 2: 
+            cout << "Feb";
+            break;
+        case 3:
+            cout << "Mar";
+            break;
+        case 4: 
+            cout << "Apr";
+            break;
+        case 5: 
+            cout << "May";
+            break;
+        case 6:
+            cout << "June";
+            break;
+        case 7: 
+            cout << "July";
+            break;
+        case 8: 
+            cout << "Aug";
+            break;
+        case 9:
+            cout << "Sep";
+            break;
+        case 10:
+            cout << "Oct";
+            break;
+        case 11:
+            cout << "Nov";
+            break;
+        case 12: 
+            cout << "Dec";
+            break;
+    } 
+
+}
